7b6bc7a72d6744655ed89e20f84fa89d88164535
achen9149 <aaronchen9149@gmail.com>
2023-11-06 18:22:40 -0500

    able to send match history info to frontend

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,16 +1,63 @@
+const regionToContinent = {
+  'AMERICAS': [ 'NA', 'BR', 'LAS', 'LAN'],
+  'ASIA': [ 'KR', 'JP'],
+  'SEA': [ 'PH', 'SG', 'TW', 'VN', 'TH'],
+  'EUROPE': [ 'EUW', 'EUN']
+}
 
 const apiController = {
   getSummonerInfo: async (req,res,next) => {
     const apiLink = `https://${req.body.region}1${process.env.IDURL}${req.body.summonerName}?api_key=${process.env.APIKEY}`
     try {
       const response = await fetch(apiLink)
-      const userInfoJSON = await response.json()
-      res.locals.userInfo = userInfoJSON;
-      return next()
+      const userInfo = await response.json()
+      res.locals.userInfo = userInfo;
+      return next();
     } catch (err) {
       return next('cannot find user:', err.message)
     }
+  },
+  getMatchHistoryCode: async (req,res,next) => {
+    let continent;
+    //convery region initial to continent name
+    for (const cont in regionToContinent) {
+      if (regionToContinent[cont].includes((req.body.region).toUpperCase())) {
+        continent = cont;
+        break;
+      }
+    }
+    const apiLink = `https://${continent}${process.env.MATCHCODEURL}${res.locals.userInfo.puuid}/ids?start=0&count=20&api_key=${process.env.APIKEY}`
+
+    try {
+      const response = await fetch(apiLink);
+      const matchCodes = await response.json();
+      res.locals.matchCodes = matchCodes;
+      res.locals.continent = continent;
+      return next();
+    } catch (err) {
+      return next('failed to find match history code:', err.message)
+    }
+  },
+  getMatchInfo: async (req,res,next) => {
+    const apiLinkArray = [];
+    for (let i = 0; i < 20; i++) {
+      const apiLink = `https://${res.locals.continent}${process.env.MATCHINFOURL}${res.locals.matchCodes[i]}?api_key=${process.env.APIKEY}`;
+      apiLinkArray.push(apiLink);
+    }
+    try {
+      const matchHistoryInfo = [];
+      for (let i = 0; i < apiLinkArray.length; i++) {
+        const response = await fetch(apiLinkArray[i]);
+        const matchInfo = await response.json();
+        matchHistoryInfo.push(matchInfo);
+      }
+      res.locals.matchHistoryInfo = matchHistoryInfo;
+      return next();
+    } catch (err) {
+      return next('failed to get match info:', err.message)
+    }
   }
+  
 }
 
 export default apiController;
--- server/route/api.js
+++ server/route/api.js
@@ -3,8 +3,8 @@ import apiController from '../controllers/apiController.js';
 
 const apiRouter = express.Router();
 
-apiRouter.post('/getUser', apiController.getSummonerInfo, (req, res) => {
-  return res.status(200).json('userHasBeenFound');
+apiRouter.post('/getUser', apiController.getSummonerInfo, apiController.getMatchHistoryCode, apiController.getMatchInfo, (req, res) => {
+  return res.status(200).json(res.locals.matchHistoryInfo);
 })
 
 export default apiRouter;
